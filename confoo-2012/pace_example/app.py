ec87b2d8e63a675b62b52740a6f2df728fac2eb2
Andy McKay <amckay@mozilla.com>
2012-03-02 11:49:01 -0500

    step 7

--- confoo-2012/pace_example/app.py
+++ confoo-2012/pace_example/app.py
@@ -9,13 +9,15 @@ import redis
 
 app = Flask(__name__)
 
+conn = redis.Redis()
 
-def listener(websocket):
+
+def listener(websocket, key):
     sub = redis.Redis().pubsub()
-    sub.subscribe('ecg')
+    sub.subscribe(key)
 
     for message in sub.listen():
-        websocket.send(message['data'])
+        websocket.send('ecg-data-%s' % message['data'])
 
 
 @app.route('/')
@@ -27,10 +29,22 @@ def index():
 def api():
     websocket = request.environ.get('wsgi.websocket', None)
     if websocket:
-        greenlet = gevent.spawn(listener, websocket)
+        greenlet = None
         while True:
             message = websocket.receive()
-            if message is None:
+            # On connected send they keys.
+            if message == 'connected':
+                websocket.send('ecgs-keys-%s' %
+                               '|'.join(conn.smembers('ecgs-keys')))
+
+            # Kill the greenlet or start a new one
+            elif message in conn.smembers('ecgs-keys'):
+                if greenlet:
+                    greenlet.kill(gevent.Greenlet.GreenletExit)
+                greenlet = gevent.spawn(listener, websocket,
+                                        'ecg-%s' % message)
+
+            elif message is None and greenlet:
                 greenlet.throw(gevent.Greenlet.GreenletExit)
                 break
 
--- confoo-2012/pace_example/ecg.py
+++ confoo-2012/pace_example/ecg.py
@@ -1,21 +1,30 @@
 import time
 import redis
 import csv
+import sys
+import os
 
 conn = redis.Redis()
 
-filename = 'ecg-data.csv'
+filename = sys.argv[1]
 lines = []
 
 read = csv.reader(open(filename))
 lines = [l for l in read]
+nice = os.path.splitext(os.path.basename(filename))[0]
+
 
 # We are doing True so that we'll have a continual flow of data from
 # our sample data.
 while True:
-    for row in lines:
-        time.sleep(0.007)
-        # JavaScript time is in ms, so we need to increase Pythons time
-        # to match JavaScript.
-        data = [str(time.time() * 1000)] + row[1:]
-        conn.publish('ecg', '|'.join(data))
+    try:
+        conn.sadd('ecgs-keys', nice)
+        for row in lines:
+            time.sleep(0.007)
+            # JavaScript time is in ms, so we need to increase Pythons time
+            # to match JavaScript.
+            data = [str(time.time() * 1000)] + row[1:]
+            conn.publish('ecg-%s' % nice, '|'.join(data))
+    except KeyboardInterrupt:
+        conn.srem('ecgs-keys', nice)
+        raise
