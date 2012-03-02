c1a02dff4755bb60e2a3cb0078fc4afea7e4f089
Andy McKay <amckay@mozilla.com>
2012-03-02 11:34:55 -0500

    step 2

--- confoo-2012/pace_example/app.py
+++ confoo-2012/pace_example/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 
+import redis
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', test=redis.Redis().get('test'))
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
